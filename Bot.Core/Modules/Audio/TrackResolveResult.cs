30333a04462e83dff34bb251bdf8e123668ea31c
Justas Dabrila <justasdabrila@gmail.com>
2016-01-11 16:28:44 +0200

    Removed redundant constructors in TrackResolveResult

--- Bot.Core/Modules/Audio/TrackResolveResult.cs
+++ Bot.Core/Modules/Audio/TrackResolveResult.cs
@@ -7,17 +7,5 @@ namespace Stormbot.Bot.Core.Modules.Audio
         public TrackData Track { get; set; }
         public bool WasSuccessful { get; set; }
         public string Message { get; set; }
-
-        public TrackResolveResult()
-        {
-            
-        }
-
-        public TrackResolveResult(TrackData track, bool wasSuccessful, string message = "")
-        {
-            Track = track;
-            WasSuccessful = wasSuccessful;
-            Message = message;
-        }
     }
 }

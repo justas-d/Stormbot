bd80d80bef612d57c7cbe448d09a2b6cc6af1a30
Justas Dabrila <justasdabrila@gmail.com>
2016-02-02 06:41:49 +0200

    Fixed the soundcloud resolver

--- Bot.Core/Modules/Audio/SoundcloudResolver.cs
+++ Bot.Core/Modules/Audio/SoundcloudResolver.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Newtonsoft.Json.Linq;
 using StrmyCore;
 
@@ -6,46 +7,42 @@ namespace Stormbot.Bot.Core.Modules.Audio
 {
     internal sealed class SoundcloudResolver : IStreamResolver
     {
+        private readonly Dictionary<string, dynamic> _cachedTrackData = new Dictionary<string, dynamic>();
+
         internal static string ApiKey;
+        private string ClientIdParam => $"?client_id={ApiKey}";
 
         public string ResolveStreamUrl(string input)
         {
-            if (ApiKey != null) return (string) GetTrackData(GetPermalink(input)).stream_url;
+            if (ApiKey != null)
+                return $"{(string) GetTrackData(input).stream_url}{ClientIdParam}";
 
             Logger.FormattedWrite(GetType().Name, "Soundcloud API Key was not set.");
             return null;
         }
 
         public bool CanResolve(string input) => input.Contains("soundcloud.com");
-        public string GetTrackName(string input) => GetTrackData(GetPermalink(input)).title;
+        public string GetTrackName(string input) => GetTrackData(input).title;
 
-        private dynamic GetTrackData(string permalink)
+        private dynamic GetTrackData(string trackUrl)
         {
             try
             {
-                // make the api request.
-                return JObject.Parse(
-                    Utils.DownloadRaw($"http://api.soundcloud.com/tracks/{permalink}?client_id={ApiKey}"));
+                if (_cachedTrackData.ContainsKey(trackUrl))
+                    return _cachedTrackData[trackUrl];
+
+                dynamic trackCallback =
+                    JObject.Parse(
+                        Utils.DownloadRaw($"http://api.soundcloud.com/resolve?url={trackUrl}&client_id={ApiKey}"));
+
+                _cachedTrackData.Add(trackUrl, trackCallback);
+
+                return trackCallback;
             }
             catch (Exception ex)
             {
                 Logger.FormattedWrite(GetType().Name, $"Failed getting sc track data. Ex: {ex}", ConsoleColor.Red);
                 return null;
-            }
-        }
-
-        private string GetPermalink(string input)
-        {
-            try
-            {
-                if (input.EndsWith("/"))
-                    input = input.Remove(input.Length - 1);
-                input = input.Replace("https://", "");
-                return input.Substring(input.LastIndexOf('/') + 1);
-            }
-            catch
-            {
-                return string.Empty;
             }
         }
     }
